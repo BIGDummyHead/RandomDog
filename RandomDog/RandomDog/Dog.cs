15d6a469a1079229d3a89aec562328a2d4a46fd2
SBFAM22 <47464673+SBFAM22@users.noreply.github.com>
2021-05-11 16:20:35 -0400

    Dogs

---
+++ RandomDog/RandomDog/Dog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace RandomDog
+{
+    /// <summary>
+    /// DOG!
+    /// </summary>
+    public sealed class Dog
+    {
+        /// <summary>
+        /// Doggy Image
+        /// </summary>
+        [JsonProperty("message")]
+        public string Message { get; private set; }
+
+        /// <summary>
+        /// The status of dog
+        /// </summary>
+        [JsonProperty("status")]
+        public string Status { get; private set; }
+
+        /// <summary>
+        /// Get Dog
+        /// </summary>
+        /// <returns></returns>
+        public static Dog Fetch()
+        {
+            using (WebClient client = new WebClient())
+            {
+                string html = client.DownloadString("https://dog.ceo/api/breeds/image/random");
+
+                return JsonConvert.DeserializeObject<Dog>(html);
+            }
+        }
+    }
+}
