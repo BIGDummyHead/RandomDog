81503f84433123253e10980862e114b4a81d247c
SBFAM22 <47464673+SBFAM22@users.noreply.github.com>
2021-05-11 20:03:02 -0400

    Dog Breed Search :smile:

--- RandomDog/RandomDog/Dog.cs
+++ RandomDog/RandomDog/Dog.cs
@@ -25,14 +25,29 @@ namespace RandomDog
         /// Get Dog
         /// </summary>
         /// <returns></returns>
-        public static Dog Fetch()
+        public static Dog Fetch(string breed = null, string subBreed = null)
         {
+            if (subBreed != null && breed == null)
+                return null;
+
             using (WebClient client = new WebClient())
             {
-                string html = client.DownloadString("https://dog.ceo/api/breeds/image/random");
+                if (breed == null)
+                {
+                    string dog = client.DownloadString("https://dog.ceo/api/breeds/image/random");
 
-                return JsonConvert.DeserializeObject<Dog>(html);
+                    return JsonConvert.DeserializeObject<Dog>(dog);
+                }
+                else if(breed != null && subBreed == null)
+                {
+                    string justBreed = client.DownloadString($"https://dog.ceo/api/breed/{breed}/images/random");
+                    return JsonConvert.DeserializeObject<Dog>(justBreed);
+                }
+
+                string sub = client.DownloadString($"https://dog.ceo/api/breed/{breed}/{subBreed}/images/random");
+                return JsonConvert.DeserializeObject<Dog>(sub);
             }
         }
+
     }
 }
