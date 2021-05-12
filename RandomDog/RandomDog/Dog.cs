782c61119d765fc1b3d49fd4f00c877328f72e82
SBFAM22 <47464673+SBFAM22@users.noreply.github.com>
2021-05-11 21:33:06 -0400

    Gave error code

--- RandomDog/RandomDog/Dog.cs
+++ RandomDog/RandomDog/Dog.cs
@@ -21,6 +21,9 @@ namespace RandomDog
         [JsonProperty("status")]
         public string Status { get; private set; }
 
+        [JsonProperty("code")]
+        public int Code { get; private set; } = 200;
+
         /// <summary>
         /// Get Dog
         /// </summary>
@@ -30,23 +33,36 @@ namespace RandomDog
             if (subBreed != null && breed == null)
                 return null;
 
-            using (WebClient client = new WebClient())
+            try
             {
-                if (breed == null)
+                using (WebClient client = new WebClient())
                 {
-                    string dog = client.DownloadString("https://dog.ceo/api/breeds/image/random");
+                    if (breed == null)
+                    {
+                        string dog = client.DownloadString("https://dog.ceo/api/breeds/image/random");
 
-                    return JsonConvert.DeserializeObject<Dog>(dog);
-                }
-                else if(breed != null && subBreed == null)
-                {
-                    string justBreed = client.DownloadString($"https://dog.ceo/api/breed/{breed}/images/random");
-                    return JsonConvert.DeserializeObject<Dog>(justBreed);
-                }
+                        return JsonConvert.DeserializeObject<Dog>(dog);
+                    }
+                    else if (breed != null && subBreed == null)
+                    {
+                        string justBreed = client.DownloadString($"https://dog.ceo/api/breed/{breed}/images/random");
+                        return JsonConvert.DeserializeObject<Dog>(justBreed);
+                    }
 
-                string sub = client.DownloadString($"https://dog.ceo/api/breed/{breed}/{subBreed}/images/random");
-                return JsonConvert.DeserializeObject<Dog>(sub);
+                    string sub = client.DownloadString($"https://dog.ceo/api/breed/{breed}/{subBreed}/images/random");
+                    return JsonConvert.DeserializeObject<Dog>(sub);
+                }
             }
+            catch 
+            {
+                return new Dog()
+                {
+                    Code = 404,
+                    Message = "Dog Not Found",
+                    Status = "error"
+                };
+            }
+            
         }
 
     }
